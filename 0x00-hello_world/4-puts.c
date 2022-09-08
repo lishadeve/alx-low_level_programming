c71c1f867fa920a9f1285663bb255909bac2c2de
lishadeve <erimalisha7@gmail.com>
2022-09-08 14:09:13 -0700

    C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ *
+ *	Description: prints "Programming is like building a multilingual puzzle
+ *
+*/
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                                                                                  
+
+/*
+ *                            
+ *	main - Print with proper grammar, but the outcome is a piece of art,                      
+ *
+*/                               
+
+int main(void)                              
+
+{                              
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);                                
+}
