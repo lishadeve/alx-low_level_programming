40d705c2ae295605a0bbf191994ab35283055e96
lishadeve <erimalisha7@gmail.com>
2022-09-08 14:27:32 -0700

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,8 +9,7 @@
  *	Return: 0
 */                               
 
-int main(void)                              
-
+int main(void)                            
 {                              
 	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 
