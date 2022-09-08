a84754faef803ce3a556709bc99e0088dfe97f1e
lishadeve <erimalisha7@gmail.com>
2022-09-08 14:13:50 -0700

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>                                                                                  
 
 /*
- *                            
+ *
  *	main - Print with proper grammar, but the outcome is a piece of art,                      
+ *
+ *	Description: Use printf
+ *
+ *	return: 0
  *
 */                               
 
