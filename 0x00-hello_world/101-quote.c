06499bdc8f68e71f84189a8d3991d8e7f6606cd8
lishadeve <erimalisha7@gmail.com>
2022-09-08 15:42:09 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-/*
- *
+/**
  *	main - prints to string
  *
  *	Description: Prints "and that piece of art is useful.." without puts
@@ -18,12 +17,7 @@ int main(void)
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
-	__asm__ ("syscall"
-	: "=a" (ret)
-	: "a" (syscall),
-	"D" (fd),
-	"S" (s),
-	"d" (l));
+	__asm__ ("syscall": "=a" (ret)	: "a" (syscall), "D" (fd), "S" (s), "d" (l));
 	
 	return (1);
 }
