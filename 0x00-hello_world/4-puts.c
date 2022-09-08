c94b19ecb1eb4e3720cefa5efa50b5ffe7c01948
lishadeve <erimalisha7@gmail.com>
2022-09-08 14:23:14 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-/*
- *
- *	main: Entry point
+/**
+ *	main - Entry point
  *
  *	Description: prints "Programming is like building a multilingual puzzle
  *
- *	return: 0
- *
+ *	Return: Always 0 (Success)
 */
 
 int main(void)
