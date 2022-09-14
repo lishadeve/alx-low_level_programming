2feb4fc2f83bdc1dc18bab51d3bc3ce9bbb3f8f4
lishadeve <erimalisha7@gmail.com>
2022-09-14 11:20:23 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * 	 _isalpha - entry point
- * 	 @c: Carrier variable
- * 	 Description: Checks for alphabetic character.
- * 	 Return: 1 if c is a letter, lowercase or uppercase. Else 0.
+ *	_isalpha - entry point
+ *	@c: Carrier variable
+ *	Description: Checks for alphabetic character.
+ *	Return: 1 if c is a letter, lowercase or uppercase. Else 0.
 */
 
 int _isalpha(int c)
