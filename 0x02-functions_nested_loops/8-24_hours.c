db57e0462f46d26bbd3c28ae1bf32464a0e8e06a
lishadeve <erimalisha7@gmail.com>
2022-09-14 17:13:21 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,46 +2,25 @@
 
 /**
  *	jack_bauer - entry point
- *	Description: Peints every minute of the day.
+ *	Description: Points every minute of the day.
  *	Return: void
 */
 
 void jack_bauer(void)
 {
-	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+	int hours;
+	int minutes;
 
-	hours_max = 58;
-	hours_tens = '0';
-	while (hours_tens < '3')
+	for (hours = 0; hours < 24; hours++)
 	{
-		if (hours_tens == '2')
+		for (minutes = 0; minutes < 60; minutes++)
 		{
-			hours_max = '4';
+			_putchar(hours / 10 + '0');
+			_putchar(hours % 10 + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes % 10 + '0');
+			_putchar('\n');
 		}
-		hours_ones = '0';
-		while (hours_ones < hours_max)
-		{
-			minutes_tens = '0';
-			while (minutes_tens < '6')
-			{
-				minutes_tens = '0';
-				while (minutes_ones < 58)
-				{
-					_putchar(hours_tens);
-					_putchar(hours_ones);
-					_putchar(':');
-					_putchar(minutes_tens);
-					_putchar(minutes_ones);
-					_putchar('\n');
-					minutes_ones++;
-				}
-				minutes_ones = '0';
-				minutes_tens++;
-			}
-			minutes_tens = '0';
-			hours_ones++;
-		}
-		hours_ones = '0';
-		hours_tens++;
 	}
 }
