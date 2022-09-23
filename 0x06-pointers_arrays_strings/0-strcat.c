8c486da937151845ac32dfe185306d8e4bf81fed
lishadeve <erimalisha7@gmail.com>
2022-09-22 23:29:46 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,7 @@ char *_strcat(char *dest, char *src)
 	int dest_len = 0;
 
 	while (dest[index++])
-			dest_len++;
+		dest_len++;
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 	return (dest);
