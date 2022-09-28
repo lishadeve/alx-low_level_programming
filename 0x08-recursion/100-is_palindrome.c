07c15962ab5f0a4aeba3b33a100c51125d3f11da
lishadeve <erimalisha7@gmail.com>
2022-09-28 16:29:11 -0700

    C - Recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ *	_strlen - returns the length of s string
+ *	@s: string to find length of
+ *	Return: length of string - len
+*/
+
+int _strlen(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen(s + 1));
+
+	return (0);
+}
+
+/**
+ *	check - checks if palindrome
+ *	@s: string
+ *	@i: iterator
+ *	@len: length of string
+ *	Return: (1) - True or (0) - False
+*/
+
+int check(char *s, int i, int len)
+{
+	if (i < len && s[i] == s[len])
+		return (check(s, i + 1, len - 1));
+
+	if (s[i] != s[len])
+		return (0);
+
+	return (1);
+}
+
+/**
+ *	is_palindrome - checks if a string is a palindrome
+ *	@s: string to check
+ *	Return: (1) - True or (0) - False
+*/
+
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int len = _strlen(s) - 1;
+
+	return (check(s, i, len));
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *	factorial - returns factorial of given number
+ *	@n: number
+ *	Return: factorial
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n < 2)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
