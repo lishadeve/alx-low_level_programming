d8ff9ef7cbb0e65abaa257f0418edcd1fcc3a2b5
lishadeve <erimalisha7@gmail.com>
2022-09-30 16:26:30 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *	main - program that prints its name
+ *	@argc: arguments
+ *	@argv: array of arguments
+ *	Return: 0
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *	main - adds positive numbers
+ *	@argc: number of arguments
+ *	@argv: array of arguments passed to the function
+ *	Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, j, num, sum;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (0);
+			}
+		}
+		num = atoi(argv[i]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
