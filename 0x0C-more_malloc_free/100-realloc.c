05c47d9607c69da335108ceff6748e0574d9b75d
lishadeve <erimalisha7@gmail.com>
2022-10-06 08:35:41 -0700

    C - More malloc, free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,28 +4,34 @@
 /**
  *	*_realloc - reallocates a memory block.
  *	@ptr: pointer to memory previously allocated
- *	@old_size: size of ptr
- *	@new_size: new size of ptr
+ *	@old_size: size in bytes allocated for ptr
+ *	@new_size: size in bytes of new memory block
  *
- *	Return: new_size ptr or NULL
+ *	Return: pointer to new memory block, NULL or ptr
 */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (new_size == 0 && ptr != NULL)
+	char *nptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if ((new_size == 0) && (ptr != NULL))
 	{
 		free(ptr);
 		return (NULL);
 	}
 
 	if (ptr == NULL)
-		ptr = malloc(new_size);
-
-	if (new_size = old_size)
-		return (ptr);
-
-	free(ptr);
-	ptr = malloc(new_size);
-
-	return (ptr);
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (nptr);
+		for (i = 0; i < old_size; i++)
+			nptr[i] = *((char *)ptr + 1);
+		free(ptr);
+	}
+	return (nptr);
 }
